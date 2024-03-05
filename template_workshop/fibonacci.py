0c5b40a199b87599ca1dc89a7cb3e87eb7da4d14
akatief <f.tib29@gmail.com>
2024-03-05 12:01:17 +0100

    feat: implement code and test solution

--- template_workshop/fibonacci.py
+++ template_workshop/fibonacci.py
@@ -4,5 +4,15 @@ class Fibonacci:
 
     def fib(self, n: int) -> int:
         ##### YOUR CODE HERE #####
-        return 0
+        if not isinstance(n, int):
+            raise TypeError("n must be an integer")
+        if n < 0:
+            raise ValueError("n must be a positive integer")
+
+        if n in self.mem:
+            return self.mem[n]
+        if n <= 1:
+            return n
+        self.mem[n] = self.fib(n - 1) + self.fib(n - 2)
+        return self.mem[n]
         ##########################
--- tests/test_fib.py
+++ tests/test_fib.py
@@ -9,5 +9,23 @@ def test_import():
         assert False
 
 ##### YOUR CODE HERE #####
+def test_fib_pos():
+    fib = Fibonacci()
+    assert fib.fib(0) == 0
+    assert fib.fib(1) == 1
+    assert fib.fib(2) == 1
+    assert fib.fib(3) == 2
+    assert fib.fib(6) == 8
+    assert fib.fib(10) == 55
+    assert fib.fib(15) == 610
 
+def test_fib_neg():
+    fib = Fibonacci()
+    with pytest.raises(ValueError):
+        fib.fib(-1)
+
+def test_fib_str():
+    fib = Fibonacci()
+    with pytest.raises(TypeError):
+        fib.fib("test")
 ##########################
